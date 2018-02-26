5f686e748a7dbb0b6a0c9c9b61ea826349bcf082
Adrian Salceanu <e@essenciary.com>
2018-02-26 15:00:23 +0100

    updated tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -61,13 +61,13 @@ isfile(secrets_path * "_") && mv(secrets_path * "_", secrets_path)
 
 delete!(ENV, "GITHUB_ACCESS_TOKEN")
 @testset "Adding GitHub issues" begin
-  # @testset "Successful authentication should return a GitHub.OAuth2 instance" begin
-  #   @test isa(IssueReporter.github_auth(), GitHub.OAuth2)
-  # end
+  @testset "Successful authentication should return a GitHub.OAuth2 instance" begin
+    @test_skip isa(IssueReporter.github_auth(), GitHub.OAuth2)
+  end
   @testset "Converting package name to GitHub id" begin
     @test IssueReporter.repo_id("Trie") == "JuliaArchive/Trie.jl"
   end
-  # @testset "Submitting an issue should result in a GitHub.Issue object" begin
-  #   @test isa(IssueReporter.report("Trie", "I found a bug", "Here is how you can reproduce the problem: ..."), GitHub.Issue)
-  # end
+  @testset "Submitting an issue should result in a GitHub.Issue object" begin
+    @test_skip isa(IssueReporter.report("Trie", "I found a bug", "Here is how you can reproduce the problem: ..."), GitHub.Issue)
+  end
 end
